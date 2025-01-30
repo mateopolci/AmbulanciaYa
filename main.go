d8cc4bf87f4d692409c90c073022da3fc8cecfc0
mateopolci <mateopolci@hotmail.com>
2025-01-30 20:09:04 -0300

    CORS fix v4

--- main.go
+++ main.go
@@ -42,10 +42,19 @@ func main() {
 
 	router := gin.Default()
 
-	router.Use(cors.New(cors.Config{
-        AllowOrigins:     []string{"http://localhost:5173"},
-        AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "PATCH"},
-        AllowHeaders:     []string{"Origin", "Content-Type"},
+    router.Use(cors.New(cors.Config{
+        AllowOrigins:     []string{"http://localhost:5173", "https://yourfrontenddomain.com"},
+        AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"},
+        AllowHeaders:     []string{
+            "Origin",
+            "Content-Type",
+            "Accept",
+            "Authorization",
+            "X-Requested-With",
+            "credentials",
+            "Access-Control-Allow-Credentials",
+        },
+        ExposeHeaders:    []string{"Content-Length", "Content-Type"},
         AllowCredentials: true,
         MaxAge:           12 * time.Hour,
     }))
